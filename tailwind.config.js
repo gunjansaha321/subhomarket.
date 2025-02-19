cdaa4d5f030c70a490b4c2bef4024036fb509f40
gunjansaha321 <papiyasaha1999kk@gmail.com>
2025-02-19 08:00:46 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#1e293b'
+      }
+    },
+  },
+  plugins: [],
+} 
